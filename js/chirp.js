bd7d291441962474c415c1c17f1c59b21404a395
Brendan Hazama <brendanhazama@Brendans-MacBook-Pro.local>
2017-09-15 15:41:04 -1000

    complete

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,18 @@
 function chirp(n){
-  // FIX ME
+  if(n === 1){             // 2 === 1, 1 == 1, "chirp"
+    return "chirp";
+  }else {
+    return "chirp " + chirp(--n); // chirp(3),chirp(2),
+  }
+
+  //chirp(--n);
+  console.log("chirp");
 }
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
 });
+
+//chirp + chirp(2);
+
+
